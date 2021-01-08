84f3de72567eb2c67ae4e8c89239bd3629f75f4b
Acer <Acer@DESKTOP-4E94458>
2021-01-08 12:59:11 +0530

    "This is my second commit as a Test Lead"

---
+++ src/test/java/com/qa/test/DemoTest.java
@@ -0,0 +1,43 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public void sum()
+	{
+		System.out.println("ADDITION");
+		int a=20;
+		int b=10;
+		Assert.assertEquals(30, a+b);
+	}
+	
+	@Test
+	public void substract()
+	{
+		System.out.println("SUBSTRACTION");
+		int a=20;
+		int b=10;
+		Assert.assertEquals(10, a-b);
+	}
+	
+	@Test
+	public void multiply()
+	{
+		System.out.println("MULTIPLICATION");
+		int a=20;
+		int b=10;
+		Assert.assertEquals(200, a*b) ;
+	}
+	
+	@Test
+	public void division()
+	{
+		System.out.println("DIVISION");
+		int a=20;
+		int b=10;
+		Assert.assertEquals(2, a/b);
+	}
+}
